f8fc87b66079705a1c6032e883243e38fc13b994
Rusbeh Turki <rusbeh.turki@gmail.com>
2024-09-06 23:01:41 -0400

    Initial Commit

---
+++ SongSync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SongSync
+//
+//  Created by Rusbeh Turki on 9/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SongSync/SongSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongSyncApp.swift
+//  SongSync
+//
+//  Created by Rusbeh Turki on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SongSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
